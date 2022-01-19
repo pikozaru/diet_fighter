1c06e635b65fd106c1fa4090d7c9b531bda36eb6
EC2 Default User <ec2-user@ip-172-31-6-25.us-west-2.compute.internal>
2022-01-19 04:47:19 +0000

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,7 +76,7 @@ class User extends Authenticatable implements MustVerifyEmail
         $this->notify(new \App\Notifications\VerifyEmail);
     }
     
-    public function sendPasswordResetNotification()
+    public function sendEmailResetPasswordNotification()
     {
         $this->notify(new \App\Notifications\ResetPassword);
     }
