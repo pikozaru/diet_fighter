bea7666edfe979b022e445185aef3f87eda7d61c
EC2 Default User <ec2-user@ip-172-31-25-126.us-west-2.compute.internal>
2022-08-08 10:47:00 +0000

    add google sarch all

--- resources/views/layouts/main_app.blade.php
+++ resources/views/layouts/main_app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="aQfKwrUNCt9V29t82_hS1CuW_SYwMMpQzutBxU9Dcvk" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
